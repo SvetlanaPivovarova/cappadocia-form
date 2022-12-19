fcdc6118b07fa7fbb65a566a7a58bd807eb2fed0
svetlana <pivovarova.svetlana@gmail.com>
2022-12-19 12:24:51 +0400

    [ Feat ] add user, address, account form to multiply form registration

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {Register} from "./register/register";
 
 function App() {
   return (
     <div className="App">
-      <AuthPage />
+      <Register />
     </div>
   );
 }
